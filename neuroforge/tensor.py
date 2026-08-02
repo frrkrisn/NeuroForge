ec9d4f3093cc38d4de75cd1eb2c98917d0cae01a
Krishna Agrawal <krishnaagrawal2101@gmail.com>
2026-08-03 03:41:17 +0530

    feat(tensor): implement tensor addition

--- neuroforge/tensor.py
+++ neuroforge/tensor.py
@@ -1,5 +1,6 @@
 """
 NeuroForge Tensor
+
 Author: Krishna Agrawal
 """
 
@@ -7,9 +8,16 @@ Author: Krishna Agrawal
 class Tensor:
 
     def __init__(self, data):
-
         self.data = data
 
     def __repr__(self):
-
         return f"Tensor({self.data})"
+
+    def __add__(self, other):
+
+        result = []
+
+        for i in range(len(self.data)):
+            result.append(self.data[i] + other.data[i])
+
+        return Tensor(result)
---
+++ tests/test_tensor.py
@@ -0,0 +1,11 @@
+from neuroforge.tensor import Tensor
+
+x = Tensor([1,2,3])
+
+y = Tensor([4,5,6])
+
+print(x)
+
+print(y)
+
+print(x+y)
